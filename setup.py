d6a888ae1349ab1c1e9d4e963de383ee69c872f8
John Wason <wason@wasontech.com>
2021-03-29 01:08:51 -0400

    Change homepage url to github, bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages, find_namespace_packages
 
 setup(
     name='RobotRaconteurCompanion',
-    version='0.1.3',
+    version='0.1.4',
     description='Robot Raconteur Python Companion Library',
     author='John Wason',
     author_email='wason@wasontech.com',
-    url='http://robotraconteur.com',
+    url='https://github.com/robotraconteur/robotraconteur_companion_python',
     package_dir={'': 'src'},
     packages=find_namespace_packages(where='src'),
     include_package_data=True,
