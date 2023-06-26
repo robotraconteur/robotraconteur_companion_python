03fed507a87102c06f7c93b9be39c063b59c680c
John Wason <wason@wasontech.com>
2023-06-26 17:49:46 -0400

    Use node directories for local identifiers

--- test/util/test_local_identifier_manager.py
+++ test/util/test_local_identifier_manager.py
@@ -2,15 +2,6 @@ import RobotRaconteurCompanion.Util.LocalIdentifiersManager as id_manager
 import RobotRaconteur as RR
 import RobotRaconteurCompanion as RRC
 
-def test_get_paths():
-    path = id_manager._get_user_identifier_path()
-    path2 = id_manager._get_user_run_path()
-    path3 = id_manager._get_user_identifier_path()
-
-    print(path)
-    print(path2)
-    print(path3)
-
 def test_identifier_lock():
     node = RR.RobotRaconteurNode()
     node.Init()
